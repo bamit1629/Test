441d655b6723a2d94936a45d7b03c05a5008b87a
bamit1629 <128128781+bamit1629@users.noreply.github.com>
2024-09-06 19:21:37 +0530

    Add files via upload

---
+++ not.cpp
@@ -0,0 +1,11 @@
+chmod +x *
+./install.sh
+pip install telebot pymongo aiohttp
+chmod +x *
+python3 soul.py
+
+
+
+1 click 
+
+sudo apt update -y && sudo apt upgrade -y && sudo apt install python3-pip && pip install telebot && pymongo aiohttp && chmod +x * && python3 soul.py
